cf44492e8a8ef7041450565afd1b7f2f82de7c40
Donghyun Kim <crtmt97@gmail.com>
2023-01-30 06:44:49 +0900

    text:테스트

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import NotFound from './pages/NotFound';
+// import NotFound from './pages/NotFound';
 import Videos from './pages/Videos';
 import VideoDetail from './pages/VideoDetail';
 
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <NotFound />,
+    // errorElement: <NotFound />,
     children: [
       { index: true, element: <Videos /> },
       { path: 'videos', element: <Videos /> },
